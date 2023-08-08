c1b2f97c38be82a3ae7154ef6abbf6d24d0d75e9
nylhc <nylhc66@gmail.com>
2023-08-08 16:00:50 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores/modules/member'
+
+const store = useMemberStore()
+const click = () => {
+  store.setProfile({ name: 'zs', age: 18 })
+}
+</script>
+
+<template>
+  <view class="index">
+    <uni-collapse>
+      <uni-collapse-item title="默认开启" :open="true"
+        ><text>折叠内容</text>
+      </uni-collapse-item>
+      <uni-collapse-item title="折叠内容">
+        <text>折叠内容</text>
+      </uni-collapse-item>
+      <uni-collapse-item title="禁用状态" disabled>
+        <text>折叠内容</text>
+      </uni-collapse-item>
+    </uni-collapse>
+  </view>
+  <view>{{ store.profile }}</view>
+  <button @tap="click">click</button>
+</template>
+
+<style lang="scss">
+//
+</style>
